2cfa9a95de2bfc98e635256256715399aa2021b6
Misthah <kpmisthah6@gmail.com>
2025-04-28 18:08:20 +0530

    feat: signin dto

---
+++ src/auth/dto/signIn.dto.ts
@@ -0,0 +1,14 @@
+import { IsString,IsNotEmpty,MinLength,IsEmail } from "class-validator";
+
+export class usersignIndto{
+    @IsString()
+    @IsNotEmpty()
+    username:string;
+
+    @IsNotEmpty()
+    @MinLength(6)
+    password:string
+
+    @IsEmail()
+    email:string
+}
