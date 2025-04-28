c60ada9a8398893fac792aadcc85fe7da8a1d806
Misthah <kpmisthah6@gmail.com>
2025-04-28 18:10:16 +0530

    jwt-strategy

---
+++ src/auth/strategy/jwt.strategy.ts
@@ -0,0 +1,21 @@
+import { PassportStrategy } from "@nestjs/passport";
+import {ExtractJwt,Strategy} from 'passport-jwt'
+import { Injectable } from "@nestjs/common";
+import { jwtConstants } from "../constants";
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy){
+    constructor(){
+        super({
+            jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+            ignoreExpiration: false,
+            secretOrKey: jwtConstants.secret,
+        })
+    }
+    async validate(payload:any){
+        return {userId:payload.sub,username:payload.username}
+    }
+}
+// We extract the JWT from the Authorization header.
+// We use the same secret to verify the token that was used to sign it.
+// The validate() method returns the payload of the valid token. We can access properties like payload.sub in our request handler
